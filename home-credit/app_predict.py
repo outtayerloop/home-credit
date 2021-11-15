dcb78ab7343108d699e10e88b59651b3ac651613
Wiem Chouchane <wiem.chouchane@efrei.net>
2021-11-15 21:42:58 +0100

    Updated predict app to run with command line arguments

--- home-credit/app_predict.py
+++ home-credit/app_predict.py
@@ -1,5 +1,56 @@
+import sys
+
 from src.models.predict import gradient_boosting_predict as gbp
 
 
+def has_valid_args(args):
+    """Determine whether the arguments passed are valid or not.
+
+    Keyword arguments:
+    args -- arguments passed (program name skipped)
+    """
+    truc = len(args) == 4
+    return len(args) == 4 and not has_wrongly_positioned_args(args)
+
+
+def display_help():
+    """Display a message indicating the command usage to run the prediction app entry point."""
+    print('Usage: poetry run python .\\app.predict.py -m \"MODEL\" -f CSV_FILE')
+    print('Try \'poetry run python .\\app.predict.py --help for help.\'')
+
+
+def has_wrongly_positioned_args(args):
+    """Determine whether the arguments passed are wrongly positioned.
+
+    Keyword arguments:
+    args -- arguments passed (program name skipped)
+    """
+    return args[0] != '-m' \
+           or args[2] != '-f' \
+           or has_wrong_model(args)
+
+
+def has_wrong_model(args):
+    """Determine whether the model name provided is wrong.
+
+    Keyword arguments:
+    args -- arguments passed (program name skipped)
+    """
+    return args[1] != 'gb' \
+           and args[1] != 'xgb' \
+           and args[1] != 'rf'
+
+
+def get_model_prediction():
+    """Get model prediction for given model and given CSV file path, otherwise display help message."""
+    args = sys.argv[1:]
+    if has_valid_args(args):
+        model = args[1]
+        file_path = args[3]
+        gbp.predict(model, file_path)
+    else:
+        display_help()
+
+
 if __name__ == '__main__':
-    gbp.predict()
+    get_model_prediction()
