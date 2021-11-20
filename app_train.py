c9df614c21f16a2be7b2067841e7dc08790eba4b
Wiem Chouchane <wiem.chouchane@efrei.net>
2021-11-20 18:32:50 +0100

    Added shap values display

--- app_train.py
+++ app_train.py
@@ -1,5 +1,7 @@
 import subprocess
 from src.features import build_features as bf
+from src.visualization import xgboost_xai as xai
+from src.models import retrieve_fit_model as rfm
 
 
 def train_models():
@@ -8,6 +10,13 @@ def train_models():
     bf.build_features()
     subprocess.run('mlflow run ./src/models/train/gradient_boosting')
     subprocess.run('mlflow run ./src/models/train/xgboost')
+    display_xgboost_model_shap_values()
+
+
+def display_xgboost_model_shap_values():
+    fit_xgb = rfm.get_fit_mlflow_model('xgb')
+    shap_values = xai.get_fitted_model_shapley_values(fit_xgb)
+    print(shap_values)
 
 
 if __name__ == '__main__':
--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,5 +1,5 @@
-import mlflow
 import pandas as pd
+from src.models import retrieve_fit_model as rfm
 
 
 def predict(model, file_path):
@@ -9,43 +9,10 @@ def predict(model, file_path):
     model -- chosen model
     file_path -- CSV file path
     """
-    max_acc_run_id = get_max_acc_model_run_id(model)
-    logged_model = 'runs:/' + max_acc_run_id + '/model'
-    loaded_model = mlflow.pyfunc.load_model(logged_model)
+    loaded_model = rfm.get_fit_mlflow_model(model)
     try:
         test = pd.read_csv(file_path).head()  # ./data/processed/processed_application_test.csv
         y_pred = loaded_model.predict(test)
         print(y_pred)
     except Exception:
         print('Given CSV file path ' + file_path + ' not found.')
-
-
-def get_max_acc_model_run_id(model):
-    """Get latest trained model with maximum accuracy.
-
-    Keyword arguments:
-    model -- chosen model
-    """
-    model_tag = get_model_tag(model)
-    found_models = mlflow.search_runs(
-        filter_string='tags.model_name = \'' + model_tag + '\'',
-        order_by=['attribute.start_time ASC']
-    )
-    max_acc = found_models['metrics.accuracy'].max()
-    models_with_max_acc = found_models[found_models['metrics.accuracy'] == max_acc]
-    max_acc_run_id = models_with_max_acc['run_id'].tail(1).values[0]
-    return max_acc_run_id
-
-
-def get_model_tag(model):
-    """Get model MLFlow tag.
-
-    Keyword arguments:
-    model -- chosen model
-    """
-    if model == 'gb':
-        return 'GradientBoostingClassifier'
-    elif model == 'xgb':
-        return 'XGBClassifier'
-    else:
-        return 'RandomForest'
