b1e040f4ffbbc7063f6519547600cfa0a7179012
Wiem Chouchane <wiem.chouchane@efrei.net>
2021-11-20 21:19:02 +0100

    Began single line shap visualization

--- src/visualization/xgboost_xai.py
+++ src/visualization/xgboost_xai.py
@@ -20,16 +20,35 @@ import pandas as pd
 
 
 # + [markdown] id="uHnlnHIjZtGG"
-# ## Calculating Shapley values for fitted XGBoost model
+# ## Calculating Shapley values for fit XGBoost model
 
 # + id="wo3KoVTlYMRG" pycharm={"name": "#%%\n"}
-def get_fitted_model_shapley_values(fitted_xgb_model):
-  """Return a list containing computed Shapley values from fitted XGBoost model.
+def get_fit_model_shapley_values_and_explainer(fit_xgb_model):
+  """Return a tuple with a list containing computed Shapley values from fit XGBoost model
+  and the obtained TreeExplainer.
 
   Keyword arguments:
-  fitted_xgb_model -- Fitted XGBoost model
+  fit_xgb_model -- Fitted XGBoost model
   """
-  explainer = shap.TreeExplainer(fitted_xgb_model)
+  explainer = shap.TreeExplainer(fit_xgb_model)
   data_for_prediction = pd.read_csv('./data/processed/processed_application_test.csv')
   shap_values = explainer.shap_values(data_for_prediction)
-  return shap_values
+  return shap_values, explainer
+
+
+# -
+
+# ## Vizualizing explanations for a single line in test dataset
+
+# + pycharm={"name": "#%%\n"}
+def vizualize_single_line_xai(explainer, line, line_index, shap_values):
+  """Call SHAP force_plot to display explanations for a single line in test dataset.
+
+  Keyword arguments:
+  explainer -- SHAP TreeExplainer obtained from fit XGBoost model
+  line -- single line in test dataset that will be used for prediction
+  line_index -- Test dataset line index
+  shap_values -- Computed SHAP values from XGBoost fit model
+  """
+  shap.initjs()
+  shap.force_plot(explainer.expected_value[line_index], shap_values[line_index], line)
